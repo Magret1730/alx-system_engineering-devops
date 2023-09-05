b9ef954d9ef77e6dcec342402526738fbe6d2745
‘Magret’ <oyedeleabiodun@gmail.com>
2023-09-05 12:11:53 -0230

    Solution to task 8

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,16 +1,2 @@
 #!/usr/bin/env ruby
-# puts ARGV[0].scan(/\[\+(\w+)]\,\[\+(\w+)]\,[+]/).join
-
-# Define the regular expression pattern to capture sender, receiver, and flags
-pattern = /\[from:(\S+)\] \[to:(\S+)\] \[flags:(.*?)\]/
-
-# Read the input log file line by line
-File.readlines(ARGV[0]).each do |line|
-  match_data = line.match(pattern)
-  if match_data
-    sender = match_data[1]
-    receiver = match_data[2]
-    flags = match_data[3]
-    puts "#{sender},#{receiver},#{flags}"
-  end
-end
+puts ARGV[0].scan(/\[from:(\S+)\] \[to:(\S+)\] \[flags:(.*?)\]/).join
